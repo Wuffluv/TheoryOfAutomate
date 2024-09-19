d5351243ff267919f5ed5e7dacf265960bcc985c
Wuffluv <rodion.rycha@yandex.ru>
2024-09-19 19:32:59 +0900

    1.9 лаба восстановлена

---
+++ 6 семестр/1.9 lab/1.9 lab/1.9 lab.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "../Lib/FormalLanguage.h"
+#include "../Lib/Rule.h"
+#include "../Lib/Grammar.h"
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << ("Задание 9.a") << endl;
+
+	list<Rule> dict =
+	{
+		 Rule("S", "aSbS"),
+		 Rule("S", "bSaS"),
+		 Rule("S", "E"),
+	};
+	PrintRules(dict);
+	FormalLanguage fl(dict);
+
+	cout << "Цепочка: " + fl.Translate("S") << endl;
+	Grammar gr(list<string>{ "S" }, list<string>{ "a", "b", "E" }, dict);
+
+
+
+	cout << "Деревья вывода" << endl;
+	cout << "Первое" << endl;
+	cout << (gr.MakeTree("aEbaEbE")) << endl;
+	cout << "Второе" << endl;
+	cout << gr.MakeTree("abEaEbE") << endl << endl;
+}
