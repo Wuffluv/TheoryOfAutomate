94d23b7544feec2370a1695fceb4dac2d1c89f5d
Wuffluv <rodion.rycha@yandex.ru>
2024-09-19 19:00:01 +0900

    1.5 лаба восстановлена

---
+++ 6 семестр/1.5 lab/1.5 lab/1.5 lab.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "../Lib/FormalLanguage.h"
+#include "../Lib/Rule.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Задание 5" << endl;
+	list<Rule> dict =
+	{
+		 Rule("S", "aSL"),
+		 Rule("S", "aL"),
+		 Rule("L", "Kc"),
+		 Rule("cK", "Kc"),
+		 Rule("K", "b"),
+	};
+	PrintRules(dict);
+
+	FormalLanguage fl(dict);
+	cout << "Цепочка: " + fl.Translate("S") << endl;
+	cout << "Язык: L = {a^n b^m c^k | a, b, k > 0}" << endl;
+
+
+	dict =
+	{
+		 Rule("S", "aSBc"),
+		 Rule("S", "abc"),
+		 Rule("cB", "Bc"),
+		 Rule("bB", "bb"),
+	};
+	PrintRules(dict);
+
+	FormalLanguage fl1(dict);
+	cout << "Цепочка: " + fl.Translate("S") << endl;
+	cout << "Язык: L = {a^n b^m c^k | a, b, k > 0}" << endl;
+
+	cout << "Грамматики эквиваленты т.к. они определяют один и тот же язык" << endl;
+
+}
