d7f21d031f55dfd23d663516b03a642418dd3f7b
Wuffluv <rodion.rycha@yandex.ru>
2024-09-19 19:11:51 +0900

    1.6 Лаба восстановлена

---
+++ 6 семестр/1.6 lab/1.6 lab/1.6 lab.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "../Lib/FormalLanguage.h"
+#include "../Lib/Rule.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Задание 6" << endl;
+	list<Rule> dict =
+	{
+		 Rule("S", "AB"),
+		 Rule("S", "ABS"),
+		 Rule("AB", "BA"),
+		 Rule("BA", "AB"),
+		 Rule("A", "a"),
+		 Rule("B", "b"),
+	};
+	PrintRules(dict);
+
+	FormalLanguage fl(dict);
+	cout << "Цепочка: " + fl.Translate("S") << endl;
+
+	dict =
+	{
+		 Rule("S", "ab"),
+	};
+	PrintRules(dict);
+
+	FormalLanguage fl1(dict);
+	cout << "Цепочка: " + fl.Translate("S") << endl;
+
+}
