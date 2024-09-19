840982ba21bafb9c06b51168e2d38908b006ca0f
Wuffluv <rodion.rycha@yandex.ru>
2024-09-19 19:23:30 +0900

    1.7 лаба восстановлена

---
+++ 6 семестр/1.7 lab/1.7 lab/1.7 lab.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include "../Lib/FormalLanguage.h"
+#include "../Lib/Rule.h"
+
+//две грамматики эквивалентны, если совпадают порождаемые ими языки
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+
+	cout << "Задание 7." << endl;
+	list<Rule> dict =
+	{
+		 Rule("S", "A.A"),
+		 Rule("A", "B"),
+		 Rule("A", "BA"),
+		 Rule("B", "0"),
+		 Rule("B", "1"),
+	};
+	PrintRules(dict);
+
+	FormalLanguage fl(dict);
+
+	string check1 = fl.OutputLeft();
+	cout << "Цепочка: " + fl.Translate("S") << endl;
+
+
+	list<Rule> dict1 =
+	{
+		 Rule("S", "A.0"),
+		 Rule("A", "0"),
+		 Rule("A", "1"),
+
+		 /* Rule("S", "A.0"),
+		  Rule("A", "B"),
+		  Rule("B", "0"),*/
+
+
+	};
+	PrintRules(dict1);
+
+	FormalLanguage fl1(dict1);
+	string check2 = fl1.OutputLeft();
+	cout << "Цепочка: " + fl1.Translate("S") << endl;
+
+	//cout << "Грамматика: G: ({A, B}, {0, 1, .}, P, S)";
+
+	if (check1 == check1) {
+		cout << "Грамматики эквивалентны, так как описывают один язык" << endl;
+	}
+	else
+		cout << "Грамматики не эквивалентны, так как описывают разные языки";
+}
