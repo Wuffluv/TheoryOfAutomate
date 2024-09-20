f16fce03d5293d8511e604538e59198cd56d47bd
Wuffluv <rodion.rycha@yandex.ru>
2024-09-20 11:49:50 +0900

    3 лаба восстановлена

---
+++ 6 семестр/3 lab/3 lab/3 lab.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+enum class State {
+    Normal_mode,
+    Oneliner,
+    Multiliner,
+    String_literal
+};
+
+void processFile(const std::string& inputFileName, const std::string& outputFileName) {
+    std::ifstream inputFile(inputFileName);
+    std::ofstream outputFile(outputFileName);
+
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return;
+    }
+
+    if (!outputFile) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return;
+    }
+
+    State state = State::Normal_mode;
+    bool ignoreNextChar = false;
+
+    char c;
+    while (inputFile.get(c)) {
+        switch (state) {
+        case State::Normal_mode:
+            if (c == '%') {
+                if (inputFile.peek() == '{') {
+                    state = State::Multiliner;
+                    inputFile.ignore();
+                }
+                else {
+                    state = State::Oneliner;
+                }
+            }
+            else if (c == '.') {
+                if (inputFile.peek() == '.' && inputFile.get() && inputFile.peek() == '.' && inputFile.get()) {
+                    state = State::Oneliner;
+                    inputFile.ignore();
+                    inputFile.ignore();
+                }
+                else {
+                    outputFile << c;
+                }
+            }
+            else if (c == '\'') {
+                //inputFile.ignore();
+                state = State::String_literal;
+                /*outputFile << c;*/
+            }
+            else {
+                outputFile << c;
+            }
+            break;
+
+        case State::Oneliner:
+            if (c == '\n') {
+                state = State::Normal_mode;
+                outputFile << c;
+            }
+            break;
+
+        case State::Multiliner:
+            if (c == '%' && inputFile.peek() == '}') {
+                state = State::Normal_mode;
+                inputFile.ignore();
+            }
+            break;
+
+        case State::String_literal:
+            if (ignoreNextChar) {
+                ignoreNextChar = false;
+            }
+            else {
+                if (c == '\'') {
+                    state = State::Normal_mode;
+                }
+                else {
+                    ignoreNextChar = (c == '\\');
+                }
+            }
+            break;
+        }
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    // Удаление последней одинарной кавычки
+    if (state == State::String_literal) {
+        outputFile << '\'';
+    }
+}
+
+int main() {
+    std::string inputFileName = "input.txt";
+    std::string outputFileName = "output.txt";
+    processFile(inputFileName, outputFileName);
+
+    std::cout << "Comments and string literals removed successfully." << std::endl;
+
+    return 0;
+}
---
+++ 6 семестр/3.1 lab/3.1 lab/3.1 lab.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+enum class State {
+    Normal_mode,
+    Comment_mode,
+    Multiline_comment_mode
+};
+
+void processFile(const std::string& inputFileName, const std::string& outputFileName) {
+    std::ifstream inputFile(inputFileName);
+    std::ofstream outputFile(outputFileName);
+
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return;
+    }
+
+    if (!outputFile) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return;
+    }
+
+    State state = State::Normal_mode;
+    char prevChar = '\0';
+    char currChar;
+    int multilineCommentDepth = 0;
+
+    while (inputFile.get(currChar)) {
+        switch (state) {
+        case State::Normal_mode:
+            if (prevChar == '#' && currChar != '=') {
+                state = State::Comment_mode;
+            }
+            else if (prevChar == '#' && currChar == '=') {
+                state = State::Multiline_comment_mode;
+                multilineCommentDepth = 1;
+            }
+            else {
+                outputFile << prevChar;
+            }
+            break;
+        case State::Comment_mode:
+            if (currChar == '\n') {
+                state = State::Normal_mode;
+                outputFile << currChar;
+            }
+            break;
+        case State::Multiline_comment_mode:
+            if (prevChar == '=' && currChar == '#') {
+                multilineCommentDepth--;
+                if (multilineCommentDepth == 0) {
+                    state = State::Normal_mode;
+                }
+            }
+            else if (prevChar == '#' && currChar == '=') {
+                multilineCommentDepth++;
+            }
+            break;
+        }
+
+        prevChar = currChar;
+    }
+
+    // Handle the last character
+    if (state == State::Normal_mode || state == State::Comment_mode) {
+        outputFile << prevChar;
+    }
+
+    inputFile.close();
+    outputFile.close();
+}
+
+int main() {
+    std::string inputFileName = "input1.txt";
+    std::string outputFileName = "output.txt";
+    processFile(inputFileName, outputFileName);
+
+    std::cout << "Comments removed successfully." << std::endl;
+
+    return 0;
+}
