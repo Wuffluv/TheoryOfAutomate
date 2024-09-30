3127049b7b27b2b244c0dd5c446e3137585ef397
Wuffluv <rodion.rycha@yandex.ru>
2024-09-30 12:18:13 +0900

    Сделаны по вариантам задачи

---
+++ 6 семестр/Вариант/Lab2.3/Lab2.3.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include "../Lib/NFA_to_DFA.h"
+#include "../Lib/FormalLanguage.h"
+#include "../Lib/Rule.h"
+
+using namespace std;
+
+//СОСТОЯНИЯ  0  1  2   3   4
+enum State { H, S, SA, AB, ER};// ER состояние ошибки
+
+void Analizator(const string& text) {
+    State now = H;
+    int count = 0;
+    string res = "";
+    bool reachedFinalState = false;//флаг проверки последнего состояния
+    do {
+        // Проверка выхода за пределы индексации
+        if (count < text.length()) {
+            switch (now) {
+            case H: {
+                if (text[count] == '0')
+                    now = S;
+                else if (text[count] == '1')
+                    now = SA;
+                else
+                    now = ER;
+                break;
+            }
+            case SA: {
+                if (text[count] == '0')
+                    now = AB;
+                else if (text[count] == '1')
+                    now = AB;
+                else
+                    now = ER;
+                break;
+            }
+            case AB: {
+                if (text[count] == '1')
+                    now = S;
+                else
+                    now = ER;
+                break;
+            }
+            default:
+                break;
+            }
+
+            res += to_string(now) + " ";
+            count++;
+        }
+        else
+            break;
+    }// while (now != S && text[count] != '\u00A7');
+    while (now != ER && text[count] != '\u00A7'); //now != State.S &&
+    cout << res << endl;
+
+    //проверяем было ли достугнуто конечное состояние 
+  /*  reachedFinalState = (now == S && count == text.length());
+    if (reachedFinalState)
+        cout << "Цепочка принадлежит данному языку." << std::endl;
+    else
+        cout << "Цепочка не принадлежит данному языку." << std::endl;*/
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    cout << "Задание 2.3" << endl;
+    list<Rule> dict =
+    {
+        Rule("S", "0A"),
+        Rule("S", "1B"),
+        Rule("A", "0A"),
+        Rule("B", "0B"),
+        Rule("B", "1B")
+
+    };
+
+    PrintRules(dict);
+    FormalLanguage fl(dict);
+
+    //take_input();
+    take_input_static();
+    print_output();
+    create_state_transitions(start_state);
+    print_dfa();
+
+
+
+    cout << "Анализаторы цепочек " << endl;
+    cout << "Состояния: H-0, S-1, SA-2, AB-3, ER-4" << endl;
+    cout << "Цепочка 1011    \t:";
+    Analizator("1011");
+    cout << "Цепочка 0       \t:";
+    Analizator("0");
+    cout << "Цепочка 101011  \t:";
+    Analizator("101011 ");
+    cout << endl;
+
+    cout << "G ({0,1}, {H,SA,AB,S},P,H)" << endl;
+
+    return 0;
+}
---
+++ 6 семестр/Вариант/Lab4/Lab4.cpp
@@ -0,0 +1,49 @@
+﻿#include "../Lib/Rule.h"
+#include "../Lib/Minimization.h"
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    // Знак перпендикулляра = +
+    list<Rule> dict1 =
+    {
+         Rule("S", "_A"),
+         Rule("A", "96N"),
+         Rule("A", "0D"),
+         Rule("D", "1F"),
+         Rule("D", "aD"),
+         Rule("D", "bD"),
+         Rule("F", "0D"),
+         Rule("F", "96N"),
+         Rule("B", "1A"),
+         Rule("B", "0A"),
+         Rule("B", "aB"),
+         Rule("B", "bB"),
+         Rule("A", "1B"),
+         Rule("A", "96N"),
+         Rule("E", "1A"),
+         Rule("E", "0D"),
+         
+    };
+
+    PrintRules(dict1);
+
+    // Создаем формальный язык
+    FormalLanguage language(dict1);
+
+    // Выводим формальный язык до устранения недостижимых состояний
+    std::cout << "Before elimination:" << std::endl;
+    std::cout << language.OutputLeft() << std::endl;
+
+    // Устраняем недостижимые состояния
+    language.RemoveUnreachableStates();
+
+    // Выводим формальный язык после устранения недостижимых состояний
+    std::cout << "After elimination:" << std::endl;
+    std::cout << language.OutputLeft() << std::endl;
+
+    return 0;
+}
