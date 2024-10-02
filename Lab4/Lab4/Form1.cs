872945b3f97af4675ff65f333f50534d9ea0733c
Wuffluv <rodion.rycha@yandex.ru>
2024-10-02 18:13:10 +0900

    написал, осталось добавить лампочки

--- Lab4/Lab4/Form1.cs
+++ Lab4/Lab4/Form1.cs
@@ -1,20 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab4
 {
     public partial class Form1 : Form
     {
+        private LampState currentState;
+
         public Form1()
         {
             InitializeComponent();
+            currentState = LampState.Off;
+            UpdateLampState();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Это кнопка "On"
+        {
+            if (currentState == LampState.Off)
+            {
+                currentState = LampState.On;
+            }
+            else if (currentState == LampState.On)
+            {
+                // Лампочка может перегореть
+                currentState = LampState.BurnOut;
+            }
+            UpdateLampState();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Это кнопка "Off"
+        {
+            if (currentState == LampState.On)
+            {
+                currentState = LampState.Off;
+            }
+            UpdateLampState();
+        }
+
+        private void UpdateLampState()
+        {
+            switch (currentState)
+            {
+                case LampState.Off:
+                    label1.Text = "Лампочка выключена";
+                    pictureBox1.Image = Properties.Resources.lamp_off; // Лампочка выключена
+                    break;
+                case LampState.On:
+                    label1.Text = "Лампочка включена";
+                    pictureBox1.Image = Properties.Resources.lamp_on; // Лампочка включена
+                    break;
+                case LampState.BurnOut:
+                    label1.Text = "Лампочка перегорела!";
+                    pictureBox1.Image = Properties.Resources.lamp_off; // Лампочка не горит (перегорела)
+                    button1.Enabled = false; // Лампочку нельзя снова включить
+                    break;
+            }
+        }
+
+        private enum LampState
+        {
+            Off,
+            On,
+            BurnOut
         }
     }
 }
