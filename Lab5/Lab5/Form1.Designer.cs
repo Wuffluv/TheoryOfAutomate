37828a26bf4450b4531b424f654e3764754a35c5
Wuffluv <rodion.rycha@yandex.ru>
2024-10-03 20:28:30 +0900

    Сделано

--- Lab5/Lab5/Form1.Designer.cs
+++ Lab5/Lab5/Form1.Designer.cs
@@ -29,12 +29,49 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(59, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(306, 625);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(682, 682);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Lab5/Lab5/Form1.cs
+++ Lab5/Lab5/Form1.cs
@@ -1,20 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Lab5
 {
     public partial class Form1 : Form
     {
+        // Перечисление состояний светофора
+        private enum TrafficLightState
+        {
+            Red,
+            Yellow,
+            Green
+        }
+
+        private TrafficLightState currentState; // Текущее состояние светофора
+        private int timeLeft; // Время до смены состояния
+        private string imagePath = @"C:\Users\wolfd\Desktop\Study\Теория Автоматов\Lab5\Img\"; // Путь к изображениям светофора
+        private bool isBlinking = false; // Флаг для мигания
+
         public Form1()
         {
             InitializeComponent();
+            InitializeTrafficLight();
+        }
+
+        // Инициализация светофора
+        private void InitializeTrafficLight()
+        {
+            // Устанавливаем начальное состояние (красный свет)
+            currentState = TrafficLightState.Red;
+            timeLeft = 10; // Время до смены с красного на желтый
+            label1.Text = $"Время до смены: {timeLeft} секунд";
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // Устанавливаем растяжение изображения
+            timer1.Interval = 1000; // Таймер будет срабатывать каждую секунду
+            timer1.Tick += Timer1_Tick; // Подписываемся на событие таймера
+            timer1.Start(); // Запускаем таймер
+            UpdateTrafficLightImage(); // Устанавливаем начальное изображение
+        }
+
+        // Обработчик таймера
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft--; // Уменьшаем время
+            label1.Text = $"Время до смены: {timeLeft} секунд";
+
+            // Логика мигания для зеленого и желтого света
+            if ((currentState == TrafficLightState.Green || currentState == TrafficLightState.Yellow) && timeLeft <= 3)
+            {
+                isBlinking = !isBlinking; // Переключаем состояние мигания
+                UpdateTrafficLightImage();
+            }
+
+            // Если время вышло, переключаем состояние
+            if (timeLeft <= 0)
+            {
+                SwitchTrafficLightState();
+            }
+        }
+
+        // Переключение состояния светофора
+        private void SwitchTrafficLightState()
+        {
+            switch (currentState)
+            {
+                case TrafficLightState.Red:
+                    currentState = TrafficLightState.Green; // Переход на зеленый
+                    timeLeft = 10; // Время до смены на желтый
+                    break;
+                case TrafficLightState.Green:
+                    currentState = TrafficLightState.Yellow; // Переход на желтый
+                    timeLeft = 5; // Время до смены на красный
+                    break;
+                case TrafficLightState.Yellow:
+                    currentState = TrafficLightState.Red; // Переход на красный
+                    timeLeft = 10; // Время до смены на зеленый
+                    break;
+            }
+
+            isBlinking = false; // Сбрасываем мигание
+            UpdateTrafficLightImage(); // Обновляем изображение светофора
+        }
+
+        // Метод для обновления изображения светофора в зависимости от состояния
+        private void UpdateTrafficLightImage()
+        {
+            string imageName = string.Empty;
+
+            switch (currentState)
+            {
+                case TrafficLightState.Red:
+                    imageName = "red.png"; // Красный свет (не мигает)
+                    break;
+                case TrafficLightState.Yellow:
+                    imageName = isBlinking ? "yellow_dim.png" : "yellow.png"; // Желтый свет мигает
+                    break;
+                case TrafficLightState.Green:
+                    imageName = isBlinking ? "green_dim.png" : "green.png"; // Зеленый свет мигает
+                    break;
+            }
+
+            // Если изображение пустое, очищаем PictureBox
+            if (!string.IsNullOrEmpty(imageName))
+            {
+                string fullPath = Path.Combine(imagePath, imageName); // Полный путь к изображению
+                if (File.Exists(fullPath))
+                {
+                    pictureBox1.Image = Image.FromFile(fullPath); // Загружаем изображение в PictureBox
+                }
+                else
+                {
+                    MessageBox.Show($"Изображение {imageName} не найдено по пути {fullPath}!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
