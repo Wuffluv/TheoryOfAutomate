3c7a2d70299ec9ad31264cd220240097f837ae55
Wuffluv <rodion.rycha@yandex.ru>
2024-11-16 13:29:29 +0900

    СМО теперь Сети Петри

--- SMO/SMO/Form1.cs
+++ SMO/SMO/Form1.cs
@@ -1,247 +1,230 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace SMO
 {
     public partial class Form1 : Form
     {
-        // Позиции каждого состояния на графе
-        private Dictionary<string, Point> statePositions;
-        // Очередь для хранения клиентов
-        private Queue<int> queue = new Queue<int>();
-        // Списки для обработанных и отброшенных клиентов
-        private List<int> processedClients = new List<int>();
-        private List<int> discardedClients = new List<int>();
-        // Таймер для обновления графа
-        private Timer timer = new Timer();
-        // Переменные для идентификации клиентов и их состояний
-        private int clientId = 1;
-        private int currentClientId = -1;
-        private int currentClientState = -1;
-        private Random random = new Random();
+        private PetriNet petriNet; // Сеть Петри для управления всей системой
+        private Timer timer = new Timer(); // Таймер для обновления сети в реальном времени
+        private int clientId = 1; // Идентификатор для нового клиента
+        private List<int> processedClients = new List<int>(); // Список обработанных клиентов
+        private List<int> discardedClients = new List<int>(); // Список отброшенных клиентов
 
         public Form1()
         {
             InitializeComponent();
-            SetupForm();
-            InitializeStatePositions();
+            SetupForm(); // Настраиваем элементы формы
 
-            // Установка интервала таймера и его запуск
-            timer.Interval = 1000;
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            // Создаем сеть Петри
+            InitializePetriNet();
+
+            timer.Interval = 1000; // Таймер срабатывает каждые 1000 мс
+            timer.Tick += Timer_Tick; // Привязываем обработчик таймера
+            timer.Start(); // Запускаем таймер
         }
 
-        // Метод для настройки начальных параметров формы
         private void SetupForm()
         {
+            // Настраиваем текст и обработчики для элементов управления на форме
             button1.Text = "Добавить клиента";
             button1.Click += button1_Click;
             labelProcessed.Text = "Обработанные";
             labelDiscarded.Text = "Отброшенные";
         }
 
-        // Метод для инициализации позиций состояний на графе
-        private void InitializeStatePositions()
+        private void InitializePetriNet()
         {
-            // позиции для каждого состояния
-            statePositions = new Dictionary<string, Point>
-            {
-                { "Start", new Point(50, 250) },
-                { "Processor1", new Point(200, 100) },
-                { "Processor2", new Point(500, 100) },
-                { "Queue3", new Point(200, 400) },
-                { "Queue2", new Point(350, 400) },
-                { "Queue1", new Point(500, 400) },
-                { "End", new Point(650, 250) }
-            };
+            petriNet = new PetriNet(); // Создаем объект сети Петри
+
+            // Создаем места (places) в сети Петри
+            petriNet.AddPlace("Очередь");
+            petriNet.AddPlace("Процессор1");
+            petriNet.AddPlace("Процессор2");
+            petriNet.AddPlace("Завершено");
+
+            // Создаем переходы (transitions) между местами
+            petriNet.AddTransition("ИзОчередиВПроцессор1", new[] { "Очередь" }, new[] { "Процессор1" });
+            petriNet.AddTransition("ИзОчередиВПроцессор2", new[] { "Очередь" }, new[] { "Процессор2" });
+            petriNet.AddTransition("ИзПроцессора1ВЗавершено", new[] { "Процессор1" }, new[] { "Завершено" });
+            petriNet.AddTransition("ИзПроцессора2ВЗавершено", new[] { "Процессор2" }, new[] { "Завершено" });
         }
 
-        // Обработчик кнопки для добавления клиента
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            bool isQueueUnavailable = random.Next(100) < 20;
-
-            // Если очередь недоступна, клиент отбрасывается
-            if (isQueueUnavailable)
+            // Добавляем клиента (токен) в место "Очередь"
+            if (petriNet.GetTokensCount("Очередь") < 6) // Ограничиваем длину очереди
             {
-                discardedClients.Add(clientId);
-                listBoxDiscarded.Items.Add($"{clientId++} (Очередь недоступна)");
-            }
-            // Проверка, переполнена ли очередь
-            else if (queue.Count >= 6)
-            {
-                // Добавление клиента в список отброшенных, если очередь переполнена
-                discardedClients.Add(clientId);
-                listBoxDiscarded.Items.Add($"{clientId++} (Очередь переполнена)");
+                petriNet.AddToken("Очередь", clientId++);
             }
             else
             {
-                // Добавление клиента в очередь
-                queue.Enqueue(clientId++);
+                // Если очередь переполнена, клиент добавляется в список отброшенных
+                discardedClients.Add(clientId);
+                listBoxDiscarded.Items.Add(clientId++);
             }
-            DrawStateMachine();
+
+            DrawStateMachine(); // Перерисовываем граф
         }
 
-
-        // Обработчик события таймера для обновления графа
         private void Timer_Tick(object sender, EventArgs e)
         {
-            // Если очередь не пуста и нет клиента в обработке, извлекаем нового клиента из очереди
-            if (queue.Count > 0 && currentClientState == -1)
+            // Выводим активные переходы в консоль для диагностики
+            var enabledTransitions = petriNet.GetEnabledTransitions();
+            Console.WriteLine($"Активные переходы: {string.Join(", ", enabledTransitions.Select(t => t.Name))}");
+
+            // Активируем переходы в сети Петри
+            petriNet.FireTransitions();
+
+            // Перемещаем обработанные токены в список обработанных
+            var completedTokens = petriNet.GetTokensFromPlace("Завершено");
+            foreach (var token in completedTokens)
             {
-                currentClientId = queue.Dequeue();
-                currentClientState = 0; // Устанавливаем начальное состояние "Start"
+                if (!processedClients.Contains(token))
+                {
+                    processedClients.Add(token); // Добавляем клиента в список обработанных
+                    listBoxProcessed.Items.Add(token); // Отображаем клиента в списке на форме
+                }
             }
 
-            // Если текущий клиент находится в обработке
-            if (currentClientState != -1)
-            {
-                ProcessClient(); // Переход к следующему состоянию
-                DrawStateMachine(); // Обновление графа
-            }
+            DrawStateMachine(); // Обновляем граф
         }
 
-        // Метод для обработки клиента через конечный автомат
-        private void ProcessClient()
-        {
-            // смен а  состояний конечного автомата
-            switch (currentClientState)
-            {
-                case 0:
-                    // Переход из начального состояния Start в одно из состояний
-                    currentClientState = GetNextState(new[] { 1, 2, 3, 4 });
-                    break;
-                case 1:
-                    // Переход от состояния Processor1 в Processor2 или Queue
-                    currentClientState = GetNextState(new[] { 2, 5 });
-                    break;
-                case 2:
-                case 5:
-                    // Завершение обработки и переход в End
-                    currentClientState = 6;
-                    break;
-                case 3:
-                    // Переход между очередями
-                    currentClientState = 4;
-                    break;
-                case 4:
-                    // Переход в очередь перед вторым процессором
-                    currentClientState = 5;
-                    break;
-                case 6:
-                    // Завершение обработки клиента
-                    processedClients.Add(currentClientId);
-                    listBoxProcessed.Items.Add(currentClientId);
-                    currentClientState = -1; // Клиент завершил путь по состояниям
-                    break;
-            }
-        }
-
-        // Метод для выбора следующего состояния случайным образом из списка возможных
-        private int GetNextState(int[] possibleStates)
-        {
-            return possibleStates[random.Next(possibleStates.Length)];
-        }
-
-
-
-
-
-        /// <summary>
-        /// Далее ниже отрисовка графа
-        /// </summary>
-
-        // Метод для отрисовки всего графа состояний и переходов
         private void DrawStateMachine()
         {
-            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height); // Создаем изображение для графа
             Graphics g = Graphics.FromImage(bmp);
 
-            DrawTransitions(g); // Рисуем все переходы
-            DrawStates(g); // Рисуем все состояния
+            // Рисуем места (places) на графе
+            DrawPlace(g, "Очередь", "Очередь", new Point(50, 250));
+            DrawPlace(g, "Процессор1", "Процессор 1", new Point(200, 100));
+            DrawPlace(g, "Процессор2", "Процессор 2", new Point(500, 100));
+            DrawPlace(g, "Завершено", "Завершено", new Point(650, 250));
 
-            pictureBox1.Image = bmp;
+            pictureBox1.Image = bmp; // Отображаем граф на форме
         }
 
-        // Метод для отрисовки всех переходов между состояниями
-        private void DrawTransitions(Graphics g)
+        private void DrawPlace(Graphics g, string placeName, string label, Point position)
         {
-            Pen pen = new Pen(Color.DarkSlateGray, 2);
+            // Рисуем место (круг) на графе
+            Brush brush = new SolidBrush(Color.LightSkyBlue);
+            g.FillEllipse(brush, position.X - 20, position.Y - 20, 50, 50);
+            g.DrawEllipse(Pens.Black, position.X - 20, position.Y - 20, 50, 50);
 
-            DrawTransition(g, pen, "Start", "Processor1");
-            DrawTransition(g, pen, "Start", "Processor2");
-            DrawTransition(g, pen, "Start", "Queue3");
-            DrawTransition(g, pen, "Start", "End");
-            DrawTransition(g, pen, "Queue3", "Queue2");
-            DrawTransition(g, pen, "Queue2", "Queue1");
-            DrawTransition(g, pen, "Queue1", "Processor1");
-            DrawTransition(g, pen, "Queue1", "Processor2");
-            DrawTransition(g, pen, "Processor1", "End");
-            DrawTransition(g, pen, "Processor2", "End");
+            // Отображаем текст с названием места и количеством токенов
+            g.DrawString(label, this.Font, Brushes.Black, position.X - 25, position.Y - 40);
+            g.DrawString($"Токены: {petriNet.GetTokensCount(placeName)}", this.Font, Brushes.Black, position.X - 25, position.Y + 30);
+        }
+    }
+
+    // Класс Place - Место в сети Петри
+    public class Place
+    {
+        public string Name { get; } // Название места
+        private List<int> tokens; // Список токенов в месте
+
+        public Place(string name)
+        {
+            Name = name;
+            tokens = new List<int>(); // Инициализируем список токенов
         }
 
-        // Метод для отрисовки каждого состояния на графе
-        private void DrawStates(Graphics g)
-        {
-            DrawState(g, "Start", queue.Count < 6 ? "Ожидание" : "Заполнено", Color.LightCoral, queue.Count);
-            DrawState(g, "Processor1", currentClientState == 1 ? $"{currentClientId} (6с)" : "Свободен", Color.Wheat);
-            DrawState(g, "Processor2", currentClientState == 2 ? $"{currentClientId} (3с)" : "Свободен", Color.Wheat);
-            DrawState(g, "Queue3", currentClientState == 3 ? currentClientId.ToString() : "Пусто", Color.LightSkyBlue);
-            DrawState(g, "Queue2", currentClientState == 4 ? currentClientId.ToString() : "Пусто", Color.LightSkyBlue);
-            DrawState(g, "Queue1", currentClientState == 5 ? currentClientId.ToString() : "Пусто", Color.LightSkyBlue);
-            DrawState(g, "End", "Завершено", Color.MediumSeaGreen);
-        }
+        // Добавляем токен в место
+        public void AddToken(int tokenId) => tokens.Add(tokenId);
 
-        // Метод для рисования отдельного состояния
-        private void DrawState(Graphics g, string stateName, string text, Color color, int queueCount = 0)
+        // Убираем токен из места
+        public int RemoveToken()
         {
-            Point pos = statePositions[stateName];
-            Brush brush = new SolidBrush(color);
-            g.FillEllipse(brush, pos.X - 20, pos.Y - 20, 50, 50);
-            g.DrawEllipse(Pens.Black, pos.X - 20, pos.Y - 20, 50, 50);
-            g.DrawString(text, this.Font, Brushes.Black, pos.X - 15, pos.Y - 10);
-
-            // Если это начальное состояние "Start", показываем количество клиентов в очереди
-            if (stateName == "Start" && queueCount > 0)
+            if (tokens.Count > 0)
             {
-                g.DrawString($"Очередь: {queueCount}", this.Font, Brushes.Black, pos.X - 15, pos.Y + 30);
+                int token = tokens[0];
+                tokens.RemoveAt(0); // Удаляем первый токен
+                return token;
+            }
+            return -1; // Если токенов нет, возвращаем -1
+        }
+
+        public int TokensCount => tokens.Count; // Количество токенов в месте
+        public List<int> Tokens => new List<int>(tokens); // Копия списка токенов
+    }
+
+    // Класс Transition - Переход в сети Петри
+    public class Transition
+    {
+        public string Name { get; } // Название перехода
+        private List<Place> inputPlaces; // Входные места
+        private List<Place> outputPlaces; // Выходные места
+
+        public Transition(string name, List<Place> inputs, List<Place> outputs)
+        {
+            Name = name;
+            inputPlaces = inputs;
+            outputPlaces = outputs;
+        }
+
+        // Проверяем, можно ли выполнить переход
+        public bool IsEnabled() => inputPlaces.All(p => p.TokensCount > 0);
+
+        // Выполняем переход
+        public void Fire()
+        {
+            if (IsEnabled())
+            {
+                foreach (var place in inputPlaces)
+                {
+                    int token = place.RemoveToken();
+                    if (token != -1)
+                    {
+                        foreach (var output in outputPlaces)
+                        {
+                            output.AddToken(token); // Перемещаем токен в выходное место
+                        }
+                    }
+                }
             }
         }
+    }
 
-        // Метод для рисования перехода между состояниями с добавлением стрелок
-        private void DrawTransition(Graphics g, Pen pen, string fromState, string toState)
+    // Класс PetriNet - Управляет всей сетью Петри
+    public class PetriNet
+    {
+        private Dictionary<string, Place> places; // Список мест
+        private List<Transition> transitions; // Список переходов
+
+        public PetriNet()
         {
-            Point fromPos = statePositions[fromState];
-            Point toPos = statePositions[toState];
-            g.DrawLine(pen, fromPos, toPos);
-            DrawArrow(g, fromPos, toPos, pen);
+            places = new Dictionary<string, Place>(); // Инициализируем места
+            transitions = new List<Transition>(); // Инициализируем переходы
         }
 
-        // Метод для добавления стрелок на концах переходов
-        private void DrawArrow(Graphics g, Point fromPos, Point toPos, Pen pen)
+        public void AddPlace(string name) => places[name] = new Place(name); // Добавляем место
+
+        public void AddTransition(string name, string[] inputNames, string[] outputNames)
         {
-            float arrowSize = 10;
-            float angle = (float)Math.Atan2(toPos.Y - fromPos.Y, toPos.X - fromPos.X);
+            var inputs = inputNames.Select(n => places[n]).ToList();
+            var outputs = outputNames.Select(n => places[n]).ToList();
+            transitions.Add(new Transition(name, inputs, outputs)); // Добавляем переход
+        }
 
-            PointF arrowEnd = new PointF(
-                toPos.X - 20 * (float)Math.Cos(angle),
-                toPos.Y - 20 * (float)Math.Sin(angle)
-            );
+        public void AddToken(string placeName, int tokenId) => places[placeName].AddToken(tokenId); // Добавляем токен в место
 
-            PointF arrowLeft = new PointF(
-                arrowEnd.X - arrowSize * (float)Math.Cos(angle - Math.PI / 6),
-                arrowEnd.Y - arrowSize * (float)Math.Sin(angle - Math.PI / 6)
-            );
-            PointF arrowRight = new PointF(
-                arrowEnd.X - arrowSize * (float)Math.Cos(angle + Math.PI / 6),
-                arrowEnd.Y - arrowSize * (float)Math.Sin(angle + Math.PI / 6)
-            );
+        public int GetTokensCount(string placeName) => places[placeName].TokensCount; // Получаем количество токенов в месте
 
-            g.DrawPolygon(pen, new PointF[] { arrowEnd, arrowLeft, arrowRight });
+        public List<int> GetTokensFromPlace(string placeName) => places[placeName].Tokens; // Получаем список токенов из места
+
+        // Получаем список активных переходов
+        public List<Transition> GetEnabledTransitions() => transitions.Where(t => t.IsEnabled()).ToList();
+
+        // Активируем переходы
+        public void FireTransitions()
+        {
+            foreach (var transition in GetEnabledTransitions())
+            {
+                transition.Fire(); // Выполняем активный переход
+            }
         }
     }
 }
